4ca5c11b3c52363905de7206402f35e1bf49a2fc
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-08-17 23:04:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+multiply;
+
+let random = Math.floor(Math.random() *10) + 1;
+random;
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+mod;
+
+let max = Math.max(1, 12, 14,15, 20);
+max;
